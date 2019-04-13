eb0bcc96d8d30e1b7eaf2867ad6da7da5f87dc67
Gabriel <gabrielleite1797@gmail.com>
2019-04-12 22:09:10 -0300

    novo programa

---
+++ javajovem/src/main/java/javajovem/Principal.java
@@ -0,0 +1,11 @@
+package javajovem;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		adicao calc = new adicao();
+		calc.somar();
+	}
+
+}
